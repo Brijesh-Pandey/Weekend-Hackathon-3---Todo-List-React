3ddbc7f571ef843406dee97a130645a762b84d59
Brijesh Pandey <30575594+Brijesh-Pandey@users.noreply.github.com>
2020-10-31 18:52:06 +0530

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import List from "./List";
 import "./../styles/App.css";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+function App() {
+  const [task, setTask] = useState("");
+  const [todolist, setTodoList] = useState([]);
+  const handleClick = () => {
+    if (task !== "") {
+      const updatelist = [...todolist];
+      updatelist.push(task);
+      setTodoList(updatelist);
+      setTask("");
+      //console.log(updatelist);
+    }
+  };
+  const handletext = (event) => {
+    setTask(event.target.value);
+  };
+  const taskChange = (index1, task, check) => {
+    if (check) {
+      return;
+    }
+    const updatelist = todolist.map((item, index) => {
+      if (index === index1) {
+        item = task;
+      }
+      return item;
+    });
+    setTodoList(updatelist);
+  };
+  const onDelete = (index1) => {
+    const updatelist = todolist.filter((item, index) => index !== index1);
+    setTodoList(updatelist);
+  };
+  return (
+    <div id="main">
+      <textarea id="task" onChange={handletext} value={task}></textarea>
+      <button id="btn" onClick={handleClick}>
+        Add
+      </button>
+      <ol>
+        <List list={todolist} taskChange={taskChange} onDelete={onDelete} />
+      </ol>
+    </div>
+  );
 }
-
 
 export default App;
